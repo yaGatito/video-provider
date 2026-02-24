d0674bd6415b58d65330b2034c77d9d08db2df66
yaGatito <j.lazovskyi@gmail.com>
2026-02-24 15:54:14 +0200

    cors: user service fix

--- internal/pkg/middleware/cors.go
+++ internal/pkg/middleware/cors.go
@@ -8,7 +8,7 @@ import (
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers for all requests
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000, http://localhost:8080, http://localhost:8081")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH, HEAD")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Max-Age", "3600")
